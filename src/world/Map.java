077901c2540712380efb22924dce139a6f52f7ac
moshe zino <moshezinoha0@gmail.com>
2026-01-22 22:40:18 +0200

    assa

--- src/world/Map.java
+++ src/world/Map.java
@@ -1,4 +1,54 @@
 package world;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Map {
+    public Tile[] tiles;
+
+    public int[][] layout;
+
+    public Map() {
+        tiles = new Tile[10];
+
+        layout = new int[][] {
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1}
+        };
+
+
+    }
+
+    private void setup(int index, String fileName, boolean walkable) {
+        try {
+            BufferedImage img = ImageIO.read(getClass().getResourceAsStream(fileName));
+            tiles[index] = new Tile(img, walkable);
+        } catch (Exception e) {
+            System.out.println("Error: Could not find file: " + fileName);
+        }
+    }
+    private void loadTileImages(){
+        setup(0, "floor.png", true);
+        setup(1, "wall.png", false);
+    }
+
+    public void draw(Graphics2D g2) {
+        int tileSize = 64;
+
+        for (int row = 0; row < layout.length; row++) {
+            for (int col = 0; col < layout[row].length; col++) {
+
+                int tileIndex = layout[row][col];
+
+
+                if (tiles[tileIndex] != null) {
+                    g2.drawImage(tiles[tileIndex].getImage(), col * tileSize, row * tileSize, tileSize, tileSize, null);
+                }
+            }
+        }
+    }
 }
--- src/world/Tile.java
+++ src/world/Tile.java
@@ -1,4 +1,21 @@
 package world;
+import java.awt.image.BufferedImage;
 
 public class Tile {
+
+    private BufferedImage image;
+    private boolean walkable;
+
+    public Tile(BufferedImage image, boolean walkable) {
+        this.image = image;
+        this.walkable = walkable;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public boolean isWalkable() {
+        return walkable;
+    }
 }
