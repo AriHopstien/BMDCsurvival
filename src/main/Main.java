04130d138bac3c041d42a125ca652342bbd28f40
ari hop <arihopstien@gmail.com>
2026-01-23 02:06:06 +0200

    main

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Yeshiva Escape");
+
+        Game game = new Game();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.add(game);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        game.start();
+    }
+}
